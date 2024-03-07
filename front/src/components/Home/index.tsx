012863996912362267bad0f86e74b70b88ff6bfe
Possari <pedropossari2@gmail.com>
2024-03-07 16:59:08 -0300

    home-update
    comecei a fazer essa merda baiano

--- front/src/components/Home/index.tsx
+++ front/src/components/Home/index.tsx
@@ -1,7 +1,15 @@
+import styles from "./Home.module.scss";
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className={styles.veiculos}>
+            <h2>Veiculos em Funcionamento</h2>
+        <nav className={styles.veiculosNavbar}>
+        <ul>
+        <li><a href="#">Todos</a></li>
+        <li><a href="#">Ativos</a></li>
+        <li><a href="#">Estacionados</a></li>
+      </ul>
+      </nav>
         </div>
     );
 }
